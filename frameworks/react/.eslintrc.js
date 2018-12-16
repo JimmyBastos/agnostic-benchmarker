2d83f7d93421aec81520defc67a574ac859bcfba
Jimmy Bastos <jimmy@localhost.localdomain>
2018-12-16 16:38:27 -0300

    [REACT APP] create-react-app

---
+++ frameworks/react/.eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  root: true,
+
+  env: {
+    node: true
+  },
+
+  extends: [
+    'standard',
+    'standard-react'
+  ],
+
+  rules: {
+    'no-console': process.env.NODE_ENV !== 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'vue/html-quotes': ['warn', 'single'],
+    'no-multi-spaces': ['warn', {
+      'exceptions': {
+        'Property': true,
+        'VariableDeclarator': true,
+        'ImportDeclaration': true
+      }
+    }],
+
+    'key-spacing': ['error', {
+      'align': {
+        'beforeColon': true,
+        'afterColon': true,
+        'on': 'colon'
+      }
+    }]
+  },
+
+  parserOptions: {
+    parser: 'babel-eslint'
+  }
+}
---
+++ frameworks/react/src/store.js
@@ -0,0 +1,83 @@
+import _random  from 'lodash/random'
+import _shuffle from 'lodash/shuffle'
+
+const rgbColorFactory = (red, green, blue) => `rgb(${red}, ${green}, ${blue})`
+
+const randomColor = (lower = 0, upper = 255) =>
+  rgbColorFactory(
+    _random(lower, upper),
+    _random(lower, upper),
+    _random(lower, upper)
+  )
+
+let _currentIndex = 0
+
+function generateAmountOfColors (amount = 1, startIndex = _currentIndex) {
+  return Array(amount).fill().map((_, i) => ({
+    id    : startIndex + (++i),
+    color : randomColor()
+  }))
+}
+
+export class Store {
+  constructor () {
+    this._colorList = []
+  }
+
+  get colors () {
+    return this._colorList
+  }
+
+  set colors (colors) {
+    this._colorList = colors
+    if (Array.isArray(colors)) {
+      _currentIndex = this._colorList.length
+    }
+  }
+
+  appendColors (amount = 1) {
+    this.colors = [].concat(
+      generateAmountOfColors(amount),
+      this.colors
+    )
+  }
+
+  shuffleColors () {
+    this.colors = _shuffle(this.colors)
+  }
+
+  sortColorsById () {
+    this.colors = [...this.colors].sort((next, curr) => (next.id - curr.id))
+  }
+
+  swapColors ([idxOne, idxTwo]) {
+    const size = this.colors.length
+    if (size > idxOne && size > idxTwo) {
+      const newColorOne = this.colors[idxTwo]
+      const newColorTwo = this.colors[idxOne]
+      const newColors = this.colors.map((clr, idx) => (idx === idxOne) ? (newColorOne) : (idx === idxTwo ? newColorTwo : clr))
+      this.colors = newColors
+    }
+  }
+
+  deleteColor (colorID) {
+    const idx = this.colors.findIndex(clr => clr.id === +colorID)
+
+    if (~idx) {
+      this.colors = [].concat(
+        this.colors.slice(0, idx),
+        this.colors.slice(idx + 1)
+      )
+    }
+  }
+
+  updateColor (colorID) {
+    const idx = this.colors.findIndex(clr => clr.id === +colorID)
+    let newColors = [ ...this.colors ]; newColors[idx] = { ...newColors[idx], color: randomColor() }
+    this.colors = newColors
+  }
+
+  clearColors () {
+    this.colors = []
+  }
+}
