66a35eeb849477a2dd27995f9d9a882b6d7ec50f
Jimmy Bastos <jimmy@localhost.localdomain>
2018-12-17 00:31:36 -0300

    eslint fix

--- frameworks/vue/src/App.template.vue
+++ frameworks/vue/src/App.template.vue
@@ -6,7 +6,6 @@
 <template>
   <!-- ROOT TAG -->
   <div id='app'>
-
     <header class='app-header app-u-fixed app-u-pin-t app-u-shadow-lg'>
       <!-- APP MENU -->
       <div class='app-container app-menu pure-menu pure-menu-horizontal'>
@@ -27,7 +26,9 @@
               class='pure-button app-button'
               style='background-color: LimeGreen'
               @click.prevent='add'
-            >Inserir 1 Item</button>
+            >
+              Inserir 1 Item
+            </button>
           </div>
 
           <!-- LOAD 100 COLORS -->
@@ -37,7 +38,9 @@
               class='pure-button app-button'
               style='background-color: DodgerBlue'
               @click.prevent='add(100)'
-            >Inserir 100 Itens</button>
+            >
+              Inserir 100 Itens
+            </button>
           </div>
 
           <!-- SWAP 2 ROWS -->
@@ -47,7 +50,9 @@
               class='pure-button app-button'
               style='background-color: OrangeRed'
               @click.prevent='swap([0, colors.length - 1])'
-            >Permutar 2 Itens</button>
+            >
+              Permutar 2 Itens
+            </button>
           </div>
 
           <!-- SHUFFLE COLORS -->
@@ -57,7 +62,9 @@
               class='pure-button app-button'
               style='background-color: DarkViolet'
               @click.prevent='shuffle'
-            >Embaralhar Lista</button>
+            >
+              Embaralhar Lista
+            </button>
           </div>
 
           <!-- SORT COLORS -->
@@ -67,7 +74,9 @@
               class='pure-button app-button'
               style='background-color: FireBrick'
               @click.prevent='sort'
-            >Ordernar Lista</button>
+            >
+              Ordernar Lista
+            </button>
           </div>
 
           <!-- CLEAR COLORS -->
@@ -77,19 +86,18 @@
               class='pure-button app-button'
               style='background-color: Crimson'
               @click.prevent='clear'
-            >Limpar Lista</button>
+            >
+              Limpar Lista
+            </button>
           </div>
         </div>
-
       </div>
     </header>
 
     <!-- APP TABLE -->
     <div class='app-container app-table-wrapper'>
       <table class='app-table'>
-
         <tbody class='app-table-body'>
-
           <!-- COLOR -->
           <tr
             v-for='{ color, color: label, id } of colors'
@@ -118,9 +126,8 @@
 
             <!-- ACTION: UPDATE COLOR -->
             <td class='app-table-cell app-table-action'>
-
               <a
-                href=''
+                href='#update'
                 class='button__update pure-button app-button app-table-label'
                 @click.prevent='updateColor(id)'
               >
@@ -131,7 +138,9 @@
                   width='24'
                   height='24'
                 >
-                  <g fill='#fff'><path d='M1.7 25.2a1 1 0 0 0 1-1c0-6.065 4.935-11 11-11h24v8.964L51.4 12.2 37.7 2.236V11.2h-24a13.02 13.02 0 0 0-13 13 1 1 0 0 0 1 1zm38-19.036L48 12.2l-8.3 6.036V6.164zm10 20.036a1 1 0 0 0-1 1c0 6.065-4.935 11-11 11h-24v-8.964L0 39.2l13.7 9.964V40.2h24a13.02 13.02 0 0 0 13-13 1 1 0 0 0-1-1zm-38 19.036L3.4 39.2l8.3-6.036v12.072z' /></g>
+                  <g fill='#fff'>
+                    <path d='M1.7 25.2a1 1 0 0 0 1-1c0-6.065 4.935-11 11-11h24v8.964L51.4 12.2 37.7 2.236V11.2h-24a13.02 13.02 0 0 0-13 13 1 1 0 0 0 1 1zm38-19.036L48 12.2l-8.3 6.036V6.164zm10 20.036a1 1 0 0 0-1 1c0 6.065-4.935 11-11 11h-24v-8.964L0 39.2l13.7 9.964V40.2h24a13.02 13.02 0 0 0 13-13 1 1 0 0 0-1-1zm-38 19.036L3.4 39.2l8.3-6.036v12.072z' />
+                  </g>
                 </svg>
               </a>
             </td>
@@ -140,7 +149,7 @@
 
             <td class='app-table-cell app-table-action'>
               <a
-                href=''
+                href='#delete'
                 class='button__delete pure-button app-button app-table-label'
                 @click.prevent='deleteColor(id)'
               >
@@ -151,7 +160,9 @@
                   width='24'
                   height='24'
                 >
-                  <g fill='#fff'><path d='M29.5 51a1 1 0 0 0 1-1V17a1 1 0 1 0-2 0v33a1 1 0 0 0 1 1zm-10 0a1 1 0 0 0 1-1V17a1 1 0 1 0-2 0v33a1 1 0 0 0 1 1zm20 0a1 1 0 0 0 1-1V17a1 1 0 1 0-2 0v33a1 1 0 0 0 1 1zm13-45H38.456c-.1-1.25-.495-3.358-1.813-4.7C35.8.434 34.75 0 33.5 0h-10c-1.252 0-2.3.434-3.144 1.3-1.318 1.353-1.703 3.46-1.813 4.7H6.5a1 1 0 1 0 0 2h2.04l1.915 46.02c.037 1.722 1.1 4.98 4.908 4.98h28.272c3.8 0 4.87-3.257 4.907-4.958L50.46 8h2.04a1 1 0 1 0 0-2zM21.792 2.68C22.24 2.223 22.8 2 23.5 2h10c.7 0 1.26.223 1.708.68.805.823 1.128 2.27 1.24 3.32H20.553c.112-1.048.435-2.496 1.24-3.32zm24.752 51.3c-.006.3-.144 3.02-2.908 3.02H15.364c-2.734 0-2.898-2.717-2.9-3.042L10.542 8h37.915l-1.913 45.98z' /></g>
+                  <g fill='#fff'>
+                    <path d='M29.5 51a1 1 0 0 0 1-1V17a1 1 0 1 0-2 0v33a1 1 0 0 0 1 1zm-10 0a1 1 0 0 0 1-1V17a1 1 0 1 0-2 0v33a1 1 0 0 0 1 1zm20 0a1 1 0 0 0 1-1V17a1 1 0 1 0-2 0v33a1 1 0 0 0 1 1zm13-45H38.456c-.1-1.25-.495-3.358-1.813-4.7C35.8.434 34.75 0 33.5 0h-10c-1.252 0-2.3.434-3.144 1.3-1.318 1.353-1.703 3.46-1.813 4.7H6.5a1 1 0 1 0 0 2h2.04l1.915 46.02c.037 1.722 1.1 4.98 4.908 4.98h28.272c3.8 0 4.87-3.257 4.907-4.958L50.46 8h2.04a1 1 0 1 0 0-2zM21.792 2.68C22.24 2.223 22.8 2 23.5 2h10c.7 0 1.26.223 1.708.68.805.823 1.128 2.27 1.24 3.32H20.553c.112-1.048.435-2.496 1.24-3.32zm24.752 51.3c-.006.3-.144 3.02-2.908 3.02H15.364c-2.734 0-2.898-2.717-2.9-3.042L10.542 8h37.915l-1.913 45.98z' />
+                  </g>
                 </svg>
               </a>
             </td>
