6e05b924d6875a970426966fe700f556fd50a817
Jimmy Bastos <jimmy@localhost.localdomain>
2018-12-19 23:40:35 -0300

    angular app package.json update

---
+++ frameworks/angular/src/app/app.component.ts
@@ -0,0 +1,59 @@
+import { Component } from '@angular/core';
+import { Store } from '../store';
+const store = new Store();
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass']
+})
+
+export class AppComponent {
+  title = 'angular-benchmark';
+  colors = Object.freeze(store.colors);
+  amount = 100;
+
+  prevent($evt) {
+    $evt.stopPropagation();
+    $evt.preventDefault();
+  }
+
+  add(amount = 1) {
+    store.appendColors(amount);
+    this.syncData();
+  }
+
+  shuffle() {
+    store.shuffleColors();
+    this.syncData();
+  }
+
+  sort() {
+    store.sortColorsById();
+    this.syncData();
+  }
+
+  swap(rows) {
+    store.swapColors(rows);
+    this.syncData();
+  }
+
+  updateColor(id) {
+    store.updateColor(id);
+    this.syncData();
+  }
+
+  deleteColor(id) {
+    store.deleteColor(id);
+    this.syncData();
+  }
+
+  clear() {
+    store.clearColors();
+    this.syncData();
+  }
+
+  syncData() {
+    this.colors = Object.freeze(store.colors);
+  }
+}
